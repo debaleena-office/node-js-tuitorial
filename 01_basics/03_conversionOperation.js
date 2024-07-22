12cac1b0f8a43b9df85fa69f15315d51f56d91c9
debaleena-office <131859718+debaleena-office@users.noreply.github.com>
2024-07-22 12:19:47 +0000

    operation conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,3 +34,47 @@ console.log( valueBoolean)
 
 
 
+/************************************   Operations **********************************/
+
+let value = 3
+let negativeVal = -value
+
+// console.log(negativeVal)    //output = -3
+
+// console.log(2+2)   // 4
+// console.log(2-2)   // 0
+// console.log(2*2)   //4
+// console.log(2**3)  //8
+// console.log(2/3)   // 0.66666666666666
+// console.log(2%3)   // 2
+
+
+
+console.log("1" + 2)  // 12
+console.log(1 + "2")   // 12
+console.log("1"+2+2)   //122
+console.log(1+2+"2")  // 32 when string is last firstly calculate first two
+console.log(1+3+3+3+"9")  // 109
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)   // 101
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`); //x:4, y:3 ,first x value is 3 that is assign in y,so y is 3 and then x incremented by 1 so x value is 4
+
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);    // a:4, b:4, first a is incremented by 1 and b value is 4 and a value is also 4
+
+
+
+
+
+
