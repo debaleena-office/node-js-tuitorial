45c4e8bd0dc5bba08f793a492edb038bbef63b8b
debaleena-office <131859718+debaleena-office@users.noreply.github.com>
2024-07-22 08:36:30 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let testConName = "";
+
+let valueName = String(testConName);
+
+//console.log(typeof valueName)
+
+// 33 => string
+// 33abc =>string
+//abc = string
+
+
+let testNumber = true;
+
+let valueNumber = Number(testNumber);
+
+//console.log(valueNumber)
+
+//33 => number
+//abc =>number
+// abc33 => number
+// true => number
+
+
+let testBoolean = ""
+let valueBoolean = Boolean(testBoolean) 
+
+console.log( valueBoolean)
+
+//1 => true datatype boolean
+// 0 => false , datatype boolean
+// "abc" =>true ,datatype boolean
+// ""  => false
+
+
+
+
