2923c187003128bb3f0e818cbefa3e953009c8d2
debaleena-office <131859718+debaleena-office@users.noreply.github.com>
2024-07-22 07:57:26 +0000

    datatype and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountName = "mou"
+let accountMail = "a@gmail.com"
+var accountDis = "Midnapore"
+accountPass = "1234"
+let accountState;
+
+
+console.table([accountName,accountMail,accountDis,accountPass,accountState])
+
+
+// prefer not to use var because of issue in block scope and functional scope
+//use let or const
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";  // treat all js as newer version
+
+//number => 2 to power 53
+//bigint 
+//string => "" or ''
+//boolean =>true or false
+//null => standalone value
+// undefined
+// symbol =>unique
+
+console.log(typeof "mou");  // output is string
+
+console.log(typeof null) //output will be object, null is as object datatype
+
+console.log(typeof undefined)  // output  undefined
